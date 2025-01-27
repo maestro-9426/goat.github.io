8e087d41507a9f41840eaf3b0c3bff2ddc3cfc22
maestro-9426 <alvincet.pro@gmail.com>
2025-01-27 13:46:48 +0100

    Add files via upload

---
+++ TP commande/afficherstock.php
@@ -0,0 +1,44 @@
+<html><head></head><body>
+    <style>
+    .connexion-message {
+            text-align: center;
+            font-size: 24px;
+            color: #4CAF50;
+            margin-top: 20px;
+            font-weight: bold;
+        }
+</style>
+
+<div class="connexion-message">PARFAIT CONNEXION REUSSI</div>
+    <center>
+         <table border="1">
+            <tbody><tr>
+                <td>ID Produit</td>
+                <td>Prix</td>
+                <td>Nom Produit</td>
+                <td>Stock</td>
+            </tr>
+                        <tr>
+                <td>2</td>
+                <td>800.00</td>
+                <td>Smartphone</td>
+                <td>15</td>
+            </tr>
+                        <tr>
+                <td>3</td>
+                <td>150.00</td>
+                <td>Casque audio</td>
+                <td>30</td>
+            </tr>
+                        <tr>
+                <td>4</td>
+                <td>50.00</td>
+                <td>Clavier</td>
+                <td>50</td>
+            </tr>
+                     </tbody></table>
+    </center>
+    <br><br>
+    <center><a href="index.html">Retour Accueil</a></center>
+        
+</body></html>
